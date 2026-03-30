5f3e64f69e544d2b51d7fe2e89aa86cd71bbe930
JP <dragonginger10@gmail.com>
2026-03-30 16:49:46 -0500

    add public key for phos

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   wslDragon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID1qC6hEuKv+i7m/s6ppfCmgVCaReSEGzA3I0OmVni+e nixos@nixos-wsl";
+  phosDragon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsuTYhZ1XsXb+d/Pyph7RpkPYnE3R4xV9Usl5aH6Ood dragon@phos";
 in
 {
-  "sneaky.age".publicKeys = [ wslDragon ];
+  "sneaky.age".publicKeys = [ wslDragon phosDragon];
 }
