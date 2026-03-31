7e297709a4a48c931874a90a217b5183373fc22e
JP <dragonginger10@gmail.com>
2026-03-30 22:08:58 -0500

    fmt

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,5 +3,8 @@ let
   phosDragon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsuTYhZ1XsXb+d/Pyph7RpkPYnE3R4xV9Usl5aH6Ood dragon@phos";
 in
 {
-  "sneaky.age".publicKeys = [ wslDragon phosDragon];
+  "sneaky.age".publicKeys = [
+    wslDragon
+    phosDragon
+  ];
 }
