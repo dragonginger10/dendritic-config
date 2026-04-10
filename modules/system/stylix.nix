3301d305022d2e8ec147d1422693dcab8e950334
JP <dragonginger10@gmail.com>
2026-04-09 20:11:46 -0500

    made stylix take a variable

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -5,9 +5,13 @@
     { pkgs, ... }:
     {
       imports = [ inputs.stylix.nixosModules.stylix ];
-      stylix = {
-        enable = true;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
-      };
+      stylix =
+        let
+          theme = "eldritch";
+        in
+        {
+          enable = true;
+          base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
+        };
     };
 }
