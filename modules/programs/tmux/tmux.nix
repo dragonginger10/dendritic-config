a52d43d22308fae9fd6deed61c3bb41b8126dc11
JP <dragonginger10@gmail.com>
2026-04-17 09:46:33 -0500

    add bare bones tmux config

--- modules/programs/tmux/tmux.nix
+++ modules/programs/tmux/tmux.nix
@@ -1,1 +1,19 @@
-{ }
+{
+  flake.module.nixos.tmux = {
+    config = {
+      progams.tmux = {
+        enable = true;
+        keymode = "vi";
+        shortcut = "z";
+        baseIndex = 1;
+        escapeTime = 10;
+        terminal = "tmux-256color";
+        # plugins = with pkgs.tmuxPlugins; [];
+        extraConfig = ''
+          bind - split-window -v -c "#{pane_current_path}"
+          bind | split-window -h -c "#{pane_current_path}"
+        '';
+      };
+    };
+  };
+}
