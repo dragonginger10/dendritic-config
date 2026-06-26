a653de9f935e4f1c5b41cef43ec61fd82eed8d45
JP <dragonginger10@gmail.com>
2026-06-26 17:30:30 -0500

    possible vfio config

--- modules/services/vfio.nix
+++ modules/services/vfio.nix
@@ -1,35 +1,37 @@
+{ self, ... }:
 {
-  flake.modules.nixos =
-    { config, ... }:
+  flake.modules.nixos.vfio =
+    { pkgs, config, ... }:
     {
-      vfio =
-        { pkgs, ... }:
-        {
-          boot = {
-            kernelParams = [
-              "amd_iommu=on"
-              "kvm.ignore_msrs=1"
-            ];
-            kernelModuels = [
-              "vfio"
-              "vfio_iommu_type1"
-              "vfio_pci"
-            ];
+      imports = [ self.modules.generic.constants ];
+
+      boot = {
+        kernelParams = [
+          "amd_iommu=on"
+          "kvm.ignore_msrs=1"
+        ];
+        kernelModules = [
+          "vfio"
+          "vfio_iommu_type1"
+          "vfio_pci"
+        ];
+      };
+
+      programs.virt-manager.enable = true;
+
+      virtualisation.libvirtd = {
+        enable = true;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          runAsRoot = true;
+          swtpm.enable = true;
+          ovmf = {
+            enable = true;
+            packages = [ pkgs.OVMFFull ];
           };
-
-          programs.virt-manager.enable = true;
-
-          qemu = {
-            package = pkgs.qemu_kvm;
-            runAsRoot = true;
-            swtpm.enable = true;
-            ovmf = {
-              enable = true;
-              packages = [ pkgs.OVMFFull.df ];
-            };
-          };
-
-          users.users.${config.preferences.user.name}.extraGroups = [ "libvirtd" ];
         };
+      };
+
+      users.users.${config.preferences.user.name}.extraGroups = [ "libvirtd" ];
     };
 }
