d3c5005654ad8c1960317c36728a4f45c70556e6
JP <dragonginger10@gmail.com>
2026-07-02 08:29:20 -0500

    cinnabar config still needs work

--- modules/hosts/cinnabar/cinnabar.nix
+++ modules/hosts/cinnabar/cinnabar.nix
@@ -1,3 +1,27 @@
-{
+{ self, ... }: {
   nixosHosts.cinnabar.enable = false;
+  flake.modules.nixos."confs/cinnabar" = { pkgs, ... }: {
+    # TODO: Get config from image
+    imports = with self.modules.nixos; [
+      kongo
+    ];
+    networking = {
+      useDHCP = false;
+      usePredicatbleInterfaceNames = false;
+      interfaces.eth0.useDHCP = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      mtr
+      sysstat
+      inetutils
+    ];
+
+    services.openssh = {
+      enable = true;
+      settings.permitRootLogin = "no";
+    };
+
+    system.stateVersion = "26.05";
+  };
 }
--- modules/hosts/cinnabar/hardware.nix
+++ modules/hosts/cinnabar/hardware.nix
@@ -1,1 +1,28 @@
-{ }
+{
+  flake.modules.nixos."confs/cinnabar" = {
+    # TODO: find extgra hw config from image
+    fileSystem."/" = {
+      device = "/dev/sda";
+      fsType = "ext4";
+    };
+
+    swapDevices = [
+      { device = "/dev/sdb"; }
+    ];
+
+    boot = {
+      kernelParams = [ "console=ttyS0,19200n8" ];
+      loader.grub = {
+        enable = true;
+        forceInstall = true;
+        device = "nodev";
+        timeout = 10;
+        extraConfig = ''
+          serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+          terminal_input serial;
+          terminal_output serial
+        '';
+      };
+    };
+  };
+}
