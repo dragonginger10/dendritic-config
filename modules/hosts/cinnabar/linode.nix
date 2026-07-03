62575e40146c90565085a4a2d8e23efa7379c3f3
JP <dragonginger10@gmail.com>
2026-07-03 16:50:26 -0500

    working vps+dns '

--- modules/hosts/cinnabar/linode.nix
+++ modules/hosts/cinnabar/linode.nix
@@ -1,33 +1,83 @@
 { self, ... }: {
-  perSystem = { config, pkgs, ... }: {
+  perSystem = { pkgs, ... }: {
     terranix.terranixConfigurations.cinnabar = {
       modules = [ self.modules.terranixModules.cinnabar ];
       terraformWrapper.package = pkgs.opentofu;
       terraformWrapper.prefixText = ''
-        TF_VAR_token="$(cat /run/agenix/linode)"
-        export TF_VAR_token
+        TF_VAR_linodeapi="$(cat /run/agenix/linode)"
+        CLOUDFLARE_API_TOKEN="$(cat /run/agenix/cloudapi)"
+        export TF_VAR_linodeapi
+        export CLOUDFLARE_API_TOKEN
       '';
     };
   };
 
-  flake.modules.terranixModules.cinnabar = { config, lib, ... }: {
-    variable.token = {
-      sensitive = true;
+  flake.modules.terranixModules.cinnabar = rec {
+    variable = {
+      linodeapi.sensitive = true;
+      zone_id.default = "5f2767cb21a39e2115e3f02ca3d2ad9a";
+      account_id.default = "c48cd0a6425c220b98338b276e7d21e3";
+      domain.default = "dragonslibrary.xyz";
     };
 
-    provider.linode = {
-      token = "\${var.token}";
+    provider = {
+      linode = {
+        token = "\${var.linodeapi}";
+      };
+      cloudflare = { };
     };
 
-    resource.linode_instance.simple = {
-      label = "example";
-      image = "linode/arch";
-      region = "us-central";
-      type = "g6-standard-1";
-      authorized_keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsuTYhZ1XsXb+d/Pyph7RpkPYnE3R4xV9Usl5aH6Ood dragon@phos"
-      ];
-      root_pass = "WonderfullyInsecureLogin";
+    resource = {
+      linode_instance.vps = {
+        label = "cinnabar";
+        region = "us-central";
+        type = "g6-standard-1";
+      };
+
+      linode_instance_disk = {
+        nixos = {
+          linode_id = "\${linode_instance.vps.id}";
+          label = "nixos";
+          size = "\${linode_instance.vps.specs.0.disk - 1024}";
+          image = "private/39801950";
+          root_pass = "tottally useless password";
+        };
+        swap = {
+          linode_id = "\${linode_instance.vps.id}";
+          label = "swap";
+          size = 1024;
+          filesystem = "swap";
+        };
+      };
+
+      linode_instance_config.bootable = {
+        linode_id = "\${linode_instance.vps.id}";
+        label = "bootable";
+        booted = true;
+        kernel = "linode/grub2";
+        device = [
+          {
+            device_name = "sda";
+            disk_id = "\${linode_instance_disk.nixos.id}";
+          }
+          {
+            device_name = "sdb";
+            disk_id = "\${linode_instance_disk.swap.id}";
+          }
+        ];
+
+      };
+
+      cloudflare_dns_record.base = {
+        name = "dragonslibrary.xyz";
+        zone_id = variable.zone_id.default;
+        content = "\${tolist(linode_instance.vps.ipv4)[0]}";
+        type = "A";
+        ttl = 60;
+        proxied = false;
+        comment = "Domain verification";
+      };
     };
+
   };
 }
