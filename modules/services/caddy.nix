c454699fc0affde4f6702bc5ab88d797a5724214
JP <dragonginger10@gmail.com>
2026-07-03 17:52:26 -0500

    caddy for dragonslibrary.xyz

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -1,10 +1,31 @@
 {
-  flake.modules.nixos.caddy = {
+  flake.modules.nixos.caddy = { pkgs, ... }: {
     services.caddy = {
       enable = true;
-      virtualHosts."localhost".extraConfig = ''
-        respond "hello world"
+      virtualHosts."dragonslibrary.xyz".extraConfig = ''
+        encode gzip
+        file_server
+        root * ${
+          pkgs.runCommand "testdir" { } ''
+            mkdir "$out"
+            echo hello world > "$out/index.html"
+          ''
+        }
       '';
+    };
+
+    networking.firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        80
+        443
+      ];
+      allowedUDPPortRanges = [
+        {
+          from = 8000;
+          to = 8010;
+        }
+      ];
     };
   };
 }
