5c18b72f723981a207d625d940351973a3812808
JP <dragonginger10@gmail.com>
2026-07-08 16:34:54 -0500

    pubkey to host. sshkey secret removed

--- modules/hosts/phos/phos.nix
+++ modules/hosts/phos/phos.nix
@@ -12,6 +12,7 @@
     { pkgs, lib, ... }:
     {
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsuTYhZ1XsXb+d/Pyph7RpkPYnE3R4xV9Usl5aH6Ood dragon@phos";
       imports = with self.modules.nixos; [
         base
         nix
--- modules/users/dragon/dragon.nix
+++ modules/users/dragon/dragon.nix
@@ -12,7 +12,6 @@ in
     (self.lib.user username true)
     {
       nixos.${username} = {
-        age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsuTYhZ1XsXb+d/Pyph7RpkPYnE3R4xV9Usl5aH6Ood dragon@phos";
         secrets = {
           cloudapi = {
             rekeyFile = ./cloudapi.age;
@@ -25,11 +24,6 @@ in
             mode = "770";
             owner = username;
             group = "users";
-          };
-          sshkey = {
-            rekeyFile = ./sshkey.age;
-            path = "/home/dragon/.ssh/id_ed25519";
-            generator.script = "passphrase";
           };
         };
         imports = with self.modules.nixos; [
