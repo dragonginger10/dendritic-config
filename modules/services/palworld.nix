3313de3fda696793260faf35273999a6c767a53d
JP <dragonginger10@gmail.com>
2026-07-21 15:41:23 -0500

    palworld ports

--- modules/services/palworld.nix
+++ modules/services/palworld.nix
@@ -1,19 +1,30 @@
 {
   flake.modules.nixos = {
     # oci.virtualisation.oci-containers.backend = "docker";
-    palworld.virtualisation.oci-containers.containers = {
-      palworld = {
-        image = "gameservermanagers/gameserver:pw";
-        autoStart = true;
-        ports = [
-          "127.0.0.1:8211:8211"
-          "127.0.0.1:8211:8211/udp"
+    palworld = {
+      networking.firewall = {
+        allowedTCPPorts = [
+          8211
         ];
-        volumes = [
-          "palworld:/data"
+        allowedUDPPorts = [
+          27015
         ];
       };
-    };
+      virtualisation.oci-containers.containers = {
+        palworld = {
+          image = "gameservermanagers/gameserver:pw";
+          autoStart = true;
+          ports = [
+            "127.0.0.1:8211:8211"
+            "127.0.0.1:8211:8211/udp"
+            "127.0.0.1:27015:27015"
+          ];
+          volumes = [
+            "palworld:/data"
+          ];
+        };
+      };
 
+    };
   };
 }
