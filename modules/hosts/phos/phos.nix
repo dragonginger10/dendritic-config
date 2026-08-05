45d4eb5f2b40df5888a60dcc2d888453402db47f
JP <dragonginger10@gmail.com>
2026-08-05 18:28:46 -0500

    it builds!

--- modules/hosts/phos/phos.nix
+++ modules/hosts/phos/phos.nix
@@ -11,7 +11,10 @@
   flake.modules.nixos."confs/phos" =
     { pkgs, lib, ... }:
     {
-      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      nixpkgs = {
+        hostPlatform = lib.mkDefault "x86_64-linux";
+        overlays = [ self.overlays.default ];
+      };
       age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsuTYhZ1XsXb+d/Pyph7RpkPYnE3R4xV9Usl5aH6Ood dragon@phos";
       imports = with self.modules.nixos; [
         base
--- modules/nix/overlays.nix
+++ modules/nix/overlays.nix
@@ -3,5 +3,6 @@
   flake.overlays.default = _final: prev: {
     local = withSystem prev.stdenv.hostPlatform.system ({ config, ... }: config.packages);
     btop = prev.btop.override { rocmSupport = true; };
+    notonoto = _final.local.notonoto;
   };
 }
