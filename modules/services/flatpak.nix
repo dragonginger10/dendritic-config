02c1c1b210a5b94e6547b179dc5f3450af019a49
JP <dragonginger10@gmail.com>
2026-08-30 17:43:45 -0500

    flatpak font fix from wiki

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -1,3 +1,36 @@
 {
-  flake.modules.nixos.flatpaks.services.flatpak.enable = true;
+  flake.modules.nixos.flatpaks =
+    { pkgs, ... }:
+    let
+      mountOptions = [
+        "ro"
+        "x-gvfs-hide"
+        "resolve-symlinks"
+      ];
+    in
+    {
+      services.flatpak.enable = true;
+
+      fonts.fontDir.enable = true;
+      system.fsPackages = [ pkgs.bindfs ];
+
+      fileSystems = {
+        "/usr/share/fonts" = {
+          device = "/run/current-system/sw/share/X11/fonts";
+          fsType = "fuse.bindfs";
+          options = mountOptions;
+        };
+        "/usr/share/icons" = {
+          device = "/run/current-system/sw/share/icons";
+          fsType = "fuse.bindfs";
+          options = mountOptions;
+        };
+        "/usr/share/themes" = {
+          device = "/run/current-system/sw/share/themes";
+          fsType = "fuse.bindfs";
+          options = mountOptions;
+        };
+      };
+
+    };
 }
